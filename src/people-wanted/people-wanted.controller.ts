f5f5ffe18c30799352b2ad08efe1eca5dca8a509
andreluas <andreluas.k@gmail.com>
2023-09-29 10:03:55 -0300

    feat: implements params search in wanteds endpoint

--- src/people-wanted/people-wanted.controller.ts
+++ src/people-wanted/people-wanted.controller.ts
@@ -1,18 +1,32 @@
-import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  NotFoundException,
+  Param,
+  Query,
+} from '@nestjs/common';
 import { PeopleWanted } from '@prisma/client';
 import { PeopleWantedService } from './people-wanted.service';
 
 @Controller('people-wanted')
 export class PeopleWantedController {
-    constructor(private service: PeopleWantedService) { }
+  constructor(private service: PeopleWantedService) {}
 
-    @Get()
-    async getPeoples(): Promise<PeopleWanted[]> {
-        return this.service.getPeoples();
-    }
+  @Get()
+  async getPeoples(
+    @Query('name') name: string,
+    @Query('nickname') nickname: string,
+    @Query('nationality') nationality: string,
+  ): Promise<PeopleWanted[]> {
+    return this.service.getPeoples({
+      name,
+      nickname,
+      nationality,
+    });
+  }
 
-    @Get(':id')
-    async getPeopleById(@Param('id') id: string): Promise<PeopleWanted> {
-        return await this.service.getPeopleById(id)
-    }
+  @Get(':id')
+  async getPeopleById(@Param('id') id: string): Promise<PeopleWanted> {
+    return await this.service.getPeopleById(id);
+  }
 }
--- src/people-wanted/people-wanted.service.ts
+++ src/people-wanted/people-wanted.service.ts
@@ -4,28 +4,47 @@ import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class PeopleWantedService {
-    constructor(private service: PrismaService) { }
+  constructor(private service: PrismaService) {}
 
-    async getPeoples(): Promise<PeopleWanted[]> {
-        return this.service.peopleWanted.findMany({
-            include: {
-                crimes: true
-            }
-        })
+  async getPeoples(params: {
+    name?: string;
+    nickname?: string;
+    nationality?: string;
+  }): Promise<PeopleWanted[]> {
+    const { name, nickname, nationality } = params;
+    return this.service.peopleWanted.findMany({
+      where: {
+        name: {
+          contains: name,
+          mode: 'insensitive',
+        },
+        nickname: {
+          contains: nickname,
+          mode: 'insensitive',
+        },
+        nationality: {
+          contains: nationality,
+          mode: 'insensitive',
+        },
+      },
+      include: {
+        crimes: true,
+      },
+    });
+  }
+
+  async getPeopleById(id: string): Promise<PeopleWanted> {
+    const people = await this.service.peopleWanted.findUnique({
+      where: { id },
+      include: {
+        crimes: true,
+      },
+    });
+
+    if (!people) {
+      throw new NotFoundException(`ID ${id} - not found in database`);
     }
 
-    async getPeopleById(id: string): Promise<PeopleWanted> {
-        const people = await this.service.peopleWanted.findUnique({
-            where: { id },
-                        include: {
-                crimes: true
-            }
-        })
-
-        if (!people) {
-            throw new NotFoundException(`ID ${id} - not found in database`)
-        }
-
-        return people
-    }
+    return people;
+  }
 }
